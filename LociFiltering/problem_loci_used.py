d48013dfb1cf80c4438641099adfa64b2759bc04
ajshultz <allisonjshultz@gmail.com>
2014-03-24 13:52:01 -0400

    Changed input and output requirements for problem_loci_used.py, so that only directories are input, and changed the output to return a file on the stats rather than printing to the screen.

--- LociFiltering/problem_loci_used.py
+++ LociFiltering/problem_loci_used.py
@@ -1,43 +1,45 @@
 #! /usr/bin/env python
 
 import re, sys, os, itertools, sets, getopt        # Load standard modules I often use
-import pickle
 
 """
-This script will take the loci from a vcf file, and identify any that have been identified as a problem (either both 1 and 2 reads, or mapping to multiple loci).
+This script will take the loci from a vcf file, and identify any that have been identified as a problem with catalog_read_pairs.py output (either both 1 and 2 reads, or mapping to multiple loci).  t\Two files will be produced in the output director: Blacklist.txt which contains a list with the name of each problem locus on a single line, and ProblemStats.txt, which is a tab delimited file containing stats on how many loci were problematic within individuals, across individuals, in either categories, and the number of  loci not in either of those categories.
+
+Note that the input directory needs to contain the "MultipleLociWithinInds.txt" and "MultipleLociAcrossInds.txt" files created from catalog_read_pair.py.
 """
 
 def main(argv):
 	try:
 		opts,args = getopt.getopt(argv,'hv:w:a:b:',)
 	except getopt.GetOptError:
-		print "problem_loci_used.py -v <Path to vcf file> -w <Path to problem loci within individuals> -a <Path to problem loci across individuals> -b <Directory for blacklist results>"
+		print "problem_loci_used.py -v <Path to vcf file> -o <path to output directory (default .)> -i <path to input directory with problem files created in catalog_read_pair.py>"
 		sys.exit(2)
 			
 	vcffile = ''
-	within_ind_file = ''
-	across_ind_file =''
-	blacklist_dir = '.'
-
+	outputdir = '.'
+	inputdir = '.'
+	
 	for opt, arg in opts:
 		if opt == "-h":
-			print "catalog_read_pair.py -l <mysql host (default localhost)> -u <mysql user (default root)> -p <mysql password> -s <Stacks database name> -o <path to output directory (default .)> -n <number of samples> -m <minimum cut off depth for a stack to be included in catalogs (default 1)>"
+			print "problem_loci_used.py -v <Path to vcf file> -o <path to output directory (default .)> -i <path to input directory with problem files created in catalog_read_pair.py>"
 			sys.exit(2)
 		elif opt == "-v":
 			vcffile = arg
-		elif opt == "-w":
-			within_ind_file = arg
-		elif opt == "-a":
-			across_ind_file = arg
-		elif opt == "-b":
-			blacklist_dir = arg
+		elif opt == "-i":
+			inputdir = arg
+		elif opt == "-o":
+			outputdir = arg
 
-	blacklist_file = "%s/Blacklist.txt"%blacklist_dir
-
+	blacklist_file = "%s/Blacklist.txt"%outputdir
+	stats_file = "%s/ProblemStats.txt"%outputdir
+	within_ind_file = '%s/MultipleLociWithinInds.txt'%inputdir
+	across_ind_file ='%s/MultipleLociAcrossInds.txt'%inputdir	
+	
 
 	within_ind = open(within_ind_file,"r")
 	across_ind = open(across_ind_file,"r")
 	vcf = open(vcffile,"r")
+	stats = open(stats_file,"w")
 	withinlist = []
 
 	for line in within_ind:
@@ -67,11 +69,10 @@ def main(argv):
 
 	bad_used = set(bothlist) & set(poploci)
 
-	print "Bad loci within ind", len(set(poploci) & set(withinlist))
-	print "Bad loci across ind", len(set(poploci) & set(acrosslist))
-	print "Bad loci in either", len(bad_used)
-
-	print len(poploci) - len(bad_used)
+	stats.write("Bad loci within ind\t%d\n"%(len(set(poploci) & set(withinlist))))
+	stats.write("Bad loci across ind\t%d\n"%(len(set(poploci) & set(acrosslist))))
+	stats.write("Bad loci in either\t%d\n"%(len(bad_used)))
+	stats.write("Loci with single matches\t%d"%(len(poploci) - len(bad_used)))
 
 	blacklist = open(blacklist_file,"w")
 	for locus in bothlist:
