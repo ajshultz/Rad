d8226cabec693f3557e1e005086e64d3f834ae5b
ajshultz <allisonjshultz@gmail.com>
2014-12-01 18:04:39 -0500

    Added capability to calculate how many loci have more than 2 allleles in each individual

--- LociFiltering/excess_allele_calc.py
+++ LociFiltering/excess_allele_calc.py
@@ -88,7 +88,43 @@ GROUP BY sample_id,
 	
 	morethan2.close()
 
+
+
+
+#########################################################################################
+#Calculate the number of loci with more than 2 alleles on a sample by sample basis.\
+
+	sample_results = open("%s/LociWithMoreThan2Alleles_PerSample_mindepth%s.csv"%(outputdir,depth),"w")
+	sample_results.write("Sample,NumLoci\n")
+
+	MyConnection = MySQLdb.connect( host = mysqlhost, user = mysqluser, \
+									passwd = mysqlpasswd, db = stacksdb)
+	MyCursor = MyConnection.cursor()
+
+	SQL = """SELECT file from samples;"""
+	SQLLen = MyCursor.execute(SQL)  # returns the number of records retrieved
 	
+	numsamples = SQLLen
+	
+	for samples in range(1,numsamples+1):
+		sample_sql = """SELECT COUNT(DISTINCT catalog_id)
+FROM HFdenovo_rxstacks_m4M3n3_radtags.matches 
+WHERE catalog_id IN (SELECT *
+	FROM (SELECT catalog_id
+		FROM """+stacksdb+""".matches
+		WHERE depth > """+depth+""" AND sample_id = """+sample+"""
+		GROUP BY sample_id,tag_id
+		HAVING COUNT(tag_id) > 2)
+	AS """+stacksdb+""";"""
+	
+		sample_sql_len = MyCursor.execute(sample_sql)
+		sample_sql_ouptut = MyCursor.fetchall()
+		
+		sample_results.write("%d,%d\n"%(sample,sample_sql_output[0])
+		
+	sample_results.close()	
+	
+
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
